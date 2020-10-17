586e1ab83cd867ba60b9c1c556c5ec6bd7daa706
David Medina <davidamedina619@gmail.com>
2020-10-17 21:00:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,8 +10,8 @@ def nyc_pigeon_organizer(data)
        if !final_array[name]
          final_array[name] = {}
        end
-       if !final_array[color]
-         final_array[color] = []
+       if !final_array[name][color]
+         final_array[name][color] = []
        end 
      end
    end 
