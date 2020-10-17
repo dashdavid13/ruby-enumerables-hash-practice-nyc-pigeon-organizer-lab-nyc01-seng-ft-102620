a72e2610e861f4bbed3f6fb272fd21faf4f0bc8c
David Medina <davidamedina619@gmail.com>
2020-10-17 20:27:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,7 @@
 
 def nyc_pigeon_organizer(data)
 final_hash = {}
- data.each do |attribute_name, attributes|
+ data.each do |first_level, attributes|
    attributes.each do |attribute_value, pigeon_names |
      pigeon_names.each 
    end
